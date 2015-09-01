9176c8b174c5fc3c630c19250c0f4dff7e252bb2
keithalewis <kal@kalx.net>
2015-09-01 07:53:55 -0400

    Turn off gsl errors when not in _DEBUG mode.

--- xll_gsl.cpp
+++ xll_gsl.cpp
@@ -21,7 +21,11 @@ void xll_error_handler (const char * reason,
 
 int xll_set_error_handler(void)
 {
+#ifdef _DEBUG
 	gsl_set_error_handler(xll_error_handler);
+#else
+	gsl_set_error_handler_off();
+#endif
 
 	return 1;
 }
